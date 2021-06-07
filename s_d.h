b4ea1d101e7465f8a4becc273ac3347927801a89
binxxi <1551097460@qq.com>
2021-06-07 22:47:45 +0800

    s_g.h

--- s_d.h
+++ s_d.h
@@ -1,42 +1,49 @@
 #include <stdio.h>
 
-#define PORT 9999
+#define PORT 8888
 
 #define LOGIN 1
 #define REGISTER 2
 #define FIND_PASSWORD 3
-#define EXIT 4
+#define FIND_ANSWER 4
+#define GET_NEW_PW 5
+#define EXIT 6
 
-#define ERROR_ID 5
-#define ERROR_PASSWORD 6
-#define ERROR_REGISTER 7
-#define ERROR_NUM 8
-#define SUCCESS_LOGIN 9
-#define SUCCESS_REGISTER 10
-#define ERROR_ANSWER 11
-#define RIGHT_ANSWER 12
-#define SUCCESS_FIND 13
+#define ERROR_ID 7
+#define ERROR_PASSWORD 8
+#define ERROR_REGISTER 9
+#define ERROR_NUM 10
+#define SUCCESS_LOGIN 11
+#define SUCCESS_REGISTER 12
+#define ERROR_ANSWER 13
+#define RIGHT_ANSWER 14
+#define SUCCESS_FIND 15
 
-#define PERSONAL_SETTINGS 14
-#define ADD_FRIEND 15
-#define VIEW_FRIENDS_LIST 16
-#define CHAT_WITH_SB 17
-#define VIEW_GROUPS_LIST 18
-#define CHAT_WITH_GROUPS 19
-#define EXITT 20
+#define PERSONAL_SETTINGS 16
+#define ADD_FRIEND 17
+#define VIEW_FRIENDS_LIST 18
+#define CHAT_WITH_SB 19
+#define VIEW_GROUPS_LIST 20
+#define CHAT_WITH_GROUPS 21
+#define RETURN_LOGIN 22
 
-#define VIEW_PER_INFO 21
-#define CHANGE_NAME 22
-#define CHANGE_PASSWORD 23 
-#define CHANGE_QUESTION 24
-#define CHANGE ANSWER 25
-#define SUCCESS_CH_NA 26
+#define VIEW_PER_INFO 23
+#define CHANGE_NAME 24
+#define CHANGE_PASSWORD 25 
+#define CHANGE_QUESTION 26
+#define CHANGE_ANSWER 27
+#define RETURN_OPT1 28
+
+#define RETURN_PER_SET 29
+#define SUCCESS_CH_NA 30
+#define ERROR_CH_PW 31
+#define SUCCESS_CH_PW 32
 
 
 typedef struct Per_Info
 {
-    int choice;
     int id;
+    int choice;
     char name[20];
     char password[16];
     char question[30];
